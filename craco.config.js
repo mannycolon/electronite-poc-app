a7609ed51338291fe04f2f72ef42926a6901db4f
mannycolon <colonmanuel7@gmail.com>
2020-04-17 12:57:15 -0400

    Repo cleanup

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,23 @@
+const { ESLINT_MODES } = require('@craco/craco');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   webpack: {
     configure: {
-      target: 'electron-renderer'
+      target: 'electron-renderer',
+       // eval-source-map allows to set inline breakpoints, and step debug at statement-level.
+       devtool: 'eval-source-map',
+       node: {
+       // TRICKY: don't let webpack hard-code these
+         __dirname: false,
+         __filename: false,
+       },
+       plugins: [
+         new CopyWebpackPlugin([
+          { from: './package.json', to: 'package.json' },
+         ]),
+       ],
     }
-  }
+  },
+  eslint: { mode: ESLINT_MODES.file },
 };
