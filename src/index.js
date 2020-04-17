772820d1946989e0e6c7a3c94c4f9e05b84e2b32
mannycolon <colonmanuel7@gmail.com>
2020-04-17 13:06:48 -0400

    lint fixed files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createElectronHandler, registerLogHandler} from './logger';
+import { createElectronHandler, registerLogHandler } from './logger';
 
 // forward logs to electron's main thread
 registerLogHandler(createElectronHandler('log-event'));
